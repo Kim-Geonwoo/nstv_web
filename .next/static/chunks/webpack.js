b9b7bde644205751ed7a675056d989125df2e70e
MAkim <kgw@geonwoo.dev>
2023-04-14 22:11:49 +0900

    2.3v 페이지 메타데이터 수정 및 사이트 최적화
    사이트 메타데이터 수정, 네이버 웹마스터 도구 태그 추가 및 사이트 부분 최적화 진행

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "914caf958f2783bf"; }
+/******/ 		__webpack_require__.h = function() { return "10b77e4042f76644"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/HeadMeta.jsx
+++ pages/HeadMeta.jsx
@@ -3,21 +3,21 @@ import Head from "next/head";
 const HeadMeta = ({ title, description, url, image }) => {
   return (
     <Head>
-      <title>{title || "씨엠프로스 공식 홈페이지"}</title>
+      <title>{title || "NSTV by NSU"}</title>
       <meta
         name="description"
         content={
           description ||
-          "일산국제컨벤션고등학교 미디어 동아리 씨엠프로스 공식홈페이지 입니다."
+          "남서울대학교 중앙동아리 NSTV 공식 홈페이지입니다."
         }
       />
-      <meta name="naver-site-verification" content="666e329eae1feaa150cc3430359630b0cec4c56a" />
+      <meta name="naver-site-verification" content="9d1443fb1057f203c1700299def47a5372dba06f" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <meta property="og:title" content={title || "씨엠프로스 공식 홈페이지"} />
+      <meta property="og:title" content={title || "NSTV by NSU"} />
       <meta property="og:type" content="website" />
-      <meta property="og:url" content={url || "https://cmpros.live"} />
+      <meta property="og:url" content={url || "https://nstv.geonwoo.dev"} />
       <meta property="og:image" content={image} />
-      <meta property="og:article:author" content="씨엠프로스 공식 홈페이지" />
+      <meta property="og:article:author" content="N.S.T.V" />
     </Head>
   );
 };
